3b6a6647ab0e619ca84500c55c1e9898ce8f76ef
DeweQ <thedeweq@yandex.ru>
2025-09-28 10:21:56 +0300

    Add #set(key, value) method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -8,12 +8,12 @@ module DataStructure
     def initialize(capacity = 16, load_factor = 0.75)
       # @capacity = capacity
       @load_factor = load_factor
-      @baskets = Array.new(capacity, LinkedList::List.new)
+      @buckets = Array.new(capacity) { LinkedList::List.new }
       @length = 0
     end
 
     def capacity
-      @baskets.size
+      @buckets.size
     end
 
     def hash(key)
@@ -21,6 +21,28 @@ module DataStructure
       prime = 79
       key.each_char { |chr| hash_code = prime * hash_code + chr.ord }
       hash_code
+    end
+
+    def set(key, value)
+      bucket = bucket_by_key(key)
+      pair = find_in_bucket(bucket, key)
+      if pair.nil?
+        bucket.push([key, value])
+        @length += 1
+      else
+        pair[1] = value
+      end
+      nil
+    end
+
+    private
+
+    def bucket_by_key(key)
+      @buckets[hash(key) % capacity]
+    end
+
+    def find_in_bucket(bucket, key)
+      bucket.each { |p| return p if p in [^key, *] }
     end
   end
 end
